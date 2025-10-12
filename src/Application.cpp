422d1343f08226fb6ba34fd1539cda2e46062115
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-10-13 01:30:46 +0530

    Start Shader

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void processInput(GLFWwindow *window);
+
 int main(void)
 {
     glfwInit();
@@ -9,7 +11,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Chota Game Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Chota Game Engine", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -24,19 +26,40 @@ int main(void)
         return -1;
     }
 
-    glViewport(0, 0, 800, 800);
+    glViewport(0, 0, 800, 600);
 
     // Set the color to clear the screen with (a dark blue-ish gray)
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    }; 
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); 
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window)) 
     {
+        processInput(window);
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
